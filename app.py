5200750ab655f57dc83d0d10bdcd263ae84d3a98
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-09-04 23:50:11 +0530

    Imported some libraries

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask, request, render_template, send_file
 
+# 24 min
+
+import os
+import pdfplumber
+import docx
+import csv
+from werkzeug.utils import secure_filename
+import google.generativeai as genai
+from fpdf import FPDF
+
 app = Flask(__name__)
 
 # routes (end points)
 @app.route("/")
 def index():
     return render_template('index.html')
+
+
+@app.route("/generate", methods=['POST'])
+def generate():
+    return render_template('results.html')
 
 
 # python main
